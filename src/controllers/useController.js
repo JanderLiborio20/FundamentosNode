5ac8b54c7f5ef6ed581fb4a87aea1895ecb07442
Jander Liborio <jandernunes14@gmail.com>
2022-09-28 08:15:27 -0400

    criado rota post

--- src/controllers/useController.js
+++ src/controllers/useController.js
@@ -1,33 +1,31 @@
-const users = require('../mocks/users')
+const users = require("../mocks/users");
 
 module.exports = {
-  listUsers(request, response){
-    
+  listUsers(request, response) {
     const { order } = request.query;
 
-    const sortedUsers = users.sort((a, b) =>{
-      if(order === 'desc'){
+    const sortedUsers = users.sort((a, b) => {
+      if (order === "desc") {
         return a.id < b.id ? 1 : -1;
       }
-  
-      return a.id > b.id ? 1 : -1;
-    })
 
-    response.writeHead(200, { "Content-Type" : "application/json" });
-    response.end(JSON.stringify(sortedUsers));
+      return a.id > b.id ? 1 : -1;
+    });
+    response.send(200, sortedUsers);
   },
 
-  getUserById(request, response){
+  getUserById(request, response) {
     const { id } = request.params;
 
-    const user = users.find(user => user.id === Number(id));
-    
-    if(!user){
-      response.writeHead(400, { "Content-Type" : "application/json" });
-      response.end(JSON.stringify({ error : 'User not found'}));
-    } else {
-      response.writeHead(200, { "Content-Type" : "application/json" });
-      response.end(JSON.stringify(user));
+    const user = users.find((user) => user.id === Number(id));
+
+    if (!user) {
+      return responsee.send(400, { error: "User not found" });
     }
-  }
-}
+    response.send(200, user);
+  },
+
+  createUser(request, response) {
+    response.send(200, { ok: true });
+  },
+};
