05b5f95ef3a2a9e1206c940b0fce98b587f0636a
jander.nunes <jander.nunes@conecthus.org.br>
2022-09-28 13:55:11 -0400

    :sparkles: feat: criando user com post

--- src/controllers/useController.js
+++ src/controllers/useController.js
@@ -3,15 +3,19 @@ const users = require("../mocks/users");
 module.exports = {
   listUsers(request, response) {
     const { order } = request.query;
-
-    const sortedUsers = users.sort((a, b) => {
-      if (order === "desc") {
-        return a.id < b.id ? 1 : -1;
+    if (order) {
+      if (order === "desc" || order === "asc") {
+        const sortedUsers = users.sort((a, b) => {
+          if (order === "desc") return a.id < b.id ? 1 : -1;
+          if (order === "asc") return a.id > b.id ? 1 : -1;
+        });
+        return response.send(200, sortedUsers);
+      } else {
+        return response.send(400, { error: "User not found" });
       }
+    }
 
-      return a.id > b.id ? 1 : -1;
-    });
-    response.send(200, sortedUsers);
+    response.send(200, users);
   },
 
   getUserById(request, response) {
@@ -20,12 +24,30 @@ module.exports = {
     const user = users.find((user) => user.id === Number(id));
 
     if (!user) {
-      return responsee.send(400, { error: "User not found" });
+      return response.send(400, { error: "User not found" });
     }
     response.send(200, user);
   },
 
   createUser(request, response) {
-    response.send(200, { ok: true });
+    let body = "";
+
+    request.on("data", (chunk) => {
+      body += chunk;
+    });
+
+    request.on("end", () => {
+      body = JSON.parse(body);
+
+      const lastUserId = users[users.length - 1].id;
+
+      const newUser = {
+        id: lastUserId + 1,
+        name: body.name,
+      };
+
+      users.push(newUser);
+      response.send(200, newUser);
+    });
   },
 };
